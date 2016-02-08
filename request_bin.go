a6787f5c0ac8be269563ec7893a1a8127d9850d7
MohamedBassem <medoox240@gmail.com>
2016-02-08 22:58:37 +0100

    Fixing the package documentation

--- request_bin.go
+++ request_bin.go
@@ -1,5 +1,4 @@
 // RequestBin is a package for testing the http requests initiated from your application. It takes a function and returns back all the requests that were initiated by this function.
-
 package requestBin
 
 import (
